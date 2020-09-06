e6960bb770c1179da081bb720484684a78f8b3cf
Shimei Zhang <61378672+shimeizhang08@users.noreply.github.com>
2020-09-06 15:59:15 +0800

    Update bookController.js

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,4 +1,5 @@
 const db = require('../model/db.js');
+const assert = require('assert');
 const authorModel = require('../model/authorModel.js');
 const bookAuthorsModel = require('../model/bookAuthorsModel.js');
 const booksModel = require('../model/booksModel.js');
@@ -7,10 +8,36 @@ const ObjectId = require('mongodb').ObjectID;
 
 const bookController = {
     getBook: function(req, res){
-        var books = []; 
-        //how to retrieve books
-        res.render("productpage",{
-            bookList: books
+        var books = [];
+        var booksVersions = [];
+        var authors = []; 
+
+        //retrieve books
+        booksModel.find({}, function (err, booksresult) {
+        bookVersionsModel.find({}, function (err, versionsresult) {
+        authorModel.find({}, function (err, authorsresult) {
+            console.log("booksresult: " +booksresult);
+
+            booksresult.forEach(function (doc, err) {
+                assert.equal(null, err);
+                books.push(doc);
+            }, function() {
+                versionsresult.forEach(function (doc2, err) {
+                    assert.equal(null, err);
+                    booksVersions.push(doc2);
+                }, function() {
+                    authorsresult.forEach(function (doc3, err) {
+                        assert.equal(null, err);
+                        authors.push(doc);
+                    }, function() {
+                        res.render("productpage", {
+                            bookList: books, booksVersions, authors
+                        });
+                    });
+                });
+            });
+        });
+        });
         });
     }
 }
