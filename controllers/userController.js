d257f5cab3fb5485309268e94245ee896b141ba3
Shimei Zhang <61378672+shimeizhang08@users.noreply.github.com>
2020-09-21 20:37:39 +0800

    Update edit profile function

--- controllers/userController.js
+++ controllers/userController.js
@@ -129,6 +129,114 @@ const userController = {
             }else
                 res.send(" null: "+result);
         });
+    },
+
+    getProfile: function(req,res) {
+
+        var username = req.session.username;
+        
+        userModel.findOne({username: username}, function(err, userResult) {
+            if (userResult != null) {
+                var firstName = userResult.firstName;
+                var lastName = userResult.lastName;
+                var email = userResult.email;
+                var password = userResult.password;
+
+                console.log(firstName + " " + lastName);
+                console.log("Email: " + email);
+                console.log("Password: " + password);
+
+                res.render("editprofile", {
+                    username: username,
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email
+                });
+            }
+        });
+    }, 
+
+    postEditProfile: function(req,res) {
+        var username = req.body.username;
+        var firstName = req.body.firstName;
+        var lastName = req.body.lastName;
+        var email = req.body.email;
+        var password = req.body.password;
+        var cpassword = req.body.cpassword;
+
+        //Checks if input fields are empty and gives default input
+        userModel.findOne({username: username}, function (err, userResult) {
+            if (userResult != null) {
+                var defaultfirst = userResult.firstName;
+                var defaultlast = userResult.lastName;
+                var defaultemail = userResult.email;
+
+                if (firstName == "") {
+                    firstName = defaultfirst;
+                }
+
+                if (lastName == "") {
+                    lastName = defaultlast;
+                }
+                
+                if (email == "") {
+                    email = defaultemail;
+                }
+            }
+        });
+
+        // User wants to change the password
+        if (password != "" && cpassword != "") {
+            let hash = bcrypt.hashSync(password, saltRounds);
+            console.log("Hash: " + hash);
+
+            if(bcrypt.compareSync(cpassword, hash)) {
+                userModel.findOneAndUpdate({username: username}, {firstName: firstName, lastName: lastName, email: email, password: hash}, null, function (err, docs) { 
+                    if (err){ 
+                        console.log(err);
+                    }
+                    else {
+                        console.log("success");
+                        res.render("editprofile", {
+                            save_success: "Edit is saved successfully!",
+                            username: username,
+                            firstName: firstName,
+                            lastName: lastName,
+                            email: email
+                        });
+                    }
+                });
+            } else {
+                //Password does not match
+                res.render("editprofile", {pError: "Confirm Password does not match Password."});
+            }
+        } //Only password or confirm password has a input
+        else if ((password != "" && cpassword == "") || (password == "" && cpassword != "")){
+            res.render("editprofile", {
+                username: username,
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                passError: "Input both password and confirm password to save changes"
+            });
+        } //No input for both password and confirm pasword
+        else {
+            userModel.findOneAndUpdate({username: username}, {firstName: firstName, lastName: lastName, email: email}, null, function (err, docs) { 
+                if (err){ 
+                    console.log(err);
+                }
+                else {
+                    console.log("success");
+                    res.render("editprofile", {
+                        save_success: "Edit is saved successfully!",
+                        username: username,
+                        firstName: firstName,
+                        lastName: lastName,
+                        email: email
+                    });
+                }
+            });
+        }
     }
 }
 
