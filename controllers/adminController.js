8bed5865425e041c0c14d7b1eb4e88983e560166
iliwysfyasbysuoi <61405165+iliwysfyasbysuoi@users.noreply.github.com>
2020-09-26 16:15:49 +0800

    fixed add products

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,7 +3,7 @@ const db = require('../model/db.js');
 const books = require('../model/booksModel.js');
 const BookVersions = require('../model/bookVersionsModel.js');
 const BookAuthors = require('../model/bookAuthorsModel.js');
-const Author = require('../model/authorModel.js');
+const authorModel = require('../model/authorModel.js');
 const ObjectId = require('mongodb').ObjectID;
 
 // for image upload
@@ -79,48 +79,123 @@ const adminController = {
 
         var bookCover = req.file.filename; 
 
-        var item = {
-            book_ID:  book_ID,
-            title : title,
-            author :  author_ID, //??
-            publisher : publisher,
-            year : year,
-            category : category,
-            bookSynopsis : bookSynopsis
-        }
+
+
+
+        console.log("author: " + author);
+
+        var authorArr = author.split(',');
+        var authorIDArr = [];
+
+        console.log("authorArr.length: " + authorArr.length);
+        
+        authorArrCount = 0;
+        authorArr.forEach(function(a, err){
+
+            console.log("a: " + a.trim());
+            var aName = a.trim();
+            authorModel.findOne({aName: aName}, function(err, authorResult){
+                if(authorResult){
+                    console.log("if")
+                    // if author already exists in the db, push its _ID
+                    authorIDArr.push(authorResult._id);
+                    console.log("authorIDArr: " + authorIDArr)
+                }else{
+                    // author DOES NOT exist in the db, make an author object then push its _ID
+                    var author = new authorModel({
+                        _id : new ObjectId(),
+                        aName : aName
+                    })
+
+                    author.save();
+                    authorIDArr.push(author._id);
+                    console.log("authorIDArr: " + authorIDArr)
+                }
+
+                authorArrCount++;
+                if( authorArrCount == authorArr.length){
+
+                    var item = {
+                        book_ID:  book_ID,
+                        title : title,
+                        author :  authorIDArr, // passes the IDs
+                        publisher : publisher,
+                        year : year,
+                        category : category,
+                        bookSynopsis : bookSynopsis
+                    }
+                    
+            
+                    var item2 = {
+                        bookVersion_ID : ObjectId(),
+                        book_ID :book_ID,
+                        priceBought : priceBought,
+                        sellingPrice : sellingPrice,
+                        quality : quality,
+                        edition : edition,
+                        type : type,
+                        quantity : quantity,
+                        bookCover : '../img/'+bookCover
+                    }
+            
+            
+                    
+            
+                    db.insertOne(books, item);
+                    db.insertOne(BookVersions, item2);
+                    // db.insertOne(Author, author);
+                    
+            
+                    res.redirect('/browse');
+
+
+                }
+
+
+                
+            })
+
+
+            
+
+
+
+        })
+        
         
 
-        var item2 = {
-            bookVersion_ID : ObjectId(),
-            book_ID :book_ID,
-            priceBought : priceBought,
-            sellingPrice : sellingPrice,
-            quality : quality,
-            edition : edition,
-            type : type,
-            quantity : quantity,
-            bookCover : bookCover
-        }
+        // var item = {
+        //     book_ID:  book_ID,
+        //     title : title,
+        //     author :  author_ID, //??
+        //     publisher : publisher,
+        //     year : year,
+        //     category : category,
+        //     bookSynopsis : bookSynopsis
+        // }
         
 
-        var author ={
-            author_ID : author_ID,
-            aName : author
-        }
-        
-        /*
-        var author2 ={
-            book_ID : 1111111111111111,
-            author_ID : ObjectId(1111111111111111)
-        } 
-        */
-        db.insertOne(books, item);
-        db.insertOne(BookVersions, item2);
-        db.insertOne(Author, author);
-        //db.insertOne(BookAuthors, author2);
+        // var item2 = {
+        //     bookVersion_ID : ObjectId(),
+        //     book_ID :book_ID,
+        //     priceBought : priceBought,
+        //     sellingPrice : sellingPrice,
+        //     quality : quality,
+        //     edition : edition,
+        //     type : type,
+        //     quantity : quantity,
+        //     bookCover : '../img/'+bookCover
+        // }
+
+
         
 
-        res.redirect('/browse');
+        // // db.insertOne(books, item);
+        // // db.insertOne(BookVersions, item2);
+        // // db.insertOne(Author, author);
+        
+
+        // // res.redirect('/browse');
     }
 }
 
--- model/authorModel.js
+++ model/authorModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 const authorSchema = new Schema({
-    author_ID: {
+    _id: {
         type: Schema.Types.ObjectId,
         unique: true,
         required: [true,'Required']
@@ -11,23 +11,6 @@ const authorSchema = new Schema({
         type: String,
         required: [true,'Required']
     }
-    // ,
-    // a_fName: {
-    //     type: String,
-    //     required: [true,'Required']
-    // },
-    // a_mName: {
-    //     type: String,
-    //     required: [true,'Required']
-    // },
-    // a_lName: {
-    //     type: String,
-    //     required: [true,'Required']
-    // },
-    // suffix: {
-    //     type: String,
-    //     required: [true,'Required']
-    // }
 });
 
 module.exports = mongoose.model('Author',authorSchema);
